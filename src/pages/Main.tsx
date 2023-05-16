d03487fbfa987158896dee0590592b6872c6786c
Shawn <shawnw@vest.fyi>
2023-05-16 16:40:33 +0800

    collect email upon first use; update icon and splash screen

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,89 @@
 import { AmplifyUser } from '@aws-amplify/ui';
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonText, IonTitle, IonToolbar } from '@ionic/react';
+import {
+    InputChangeEventDetail, IonButton,
+    IonButtons,
+    IonContent,
+    IonHeader, IonInput, IonItem,
+    IonMenuButton, IonModal,
+    IonPage,
+    IonSpinner,
+    IonText,
+    IonToolbar
+} from '@ionic/react';
 import ReactWebChat, { createDirectLine } from 'botframework-webchat';
-import { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import './Main.css';
+import { Preferences } from '@capacitor/preferences';
 
 export interface MainProp {
-    user: AmplifyUser
+    user: AmplifyUser;
+    emailFromStorage?: string;
 }
 
-export const Main: React.FC<MainProp> = ({ user}) => {
-    const directLine = useMemo(() => createDirectLine({ secret: '89Et2XauBY8.6IpmRylqRtyyFdIyL800zI66bclIE6Zjj-AN7Wgtsj0' }), []);
+export const Main: React.FC<MainProp> = ({ user }) => {
+    const [ isLoading, setIsLoading ] = useState(true);
 
-    console.log('directLine obj is', JSON.stringify(directLine));
+    // -------------------
+    // Bot Framework Web Chat Config
+    // -------------------
+    const directLine = useMemo(() => createDirectLine({
+        // secret: '89Et2XauBY8.NLUFJiExmvbNiPLmpG51ECssq6LqV1WvHoatqEY_q-A'
+        secret: '89Et2XauBY8.6IpmRylqRtyyFdIyL800zI66bclIE6Zjj-AN7Wgtsj0'
+    }), []);
+
+    const styleOptions = {
+        bubbleBackground: '#F2F2F2',
+        bubbleFromUserBackground: '#D6E9FF',
+        bubbleBorderRadius: 8,
+        bubbleFromUserBorderRadius: 8,
+        hideUploadButton: true,
+        sendBoxHeight: 80,
+        fontSizeSmall: '80%',
+        adaptiveCardActionFontSize: 80,
+        suggestedActionBorderRadius: 8,
+    };
+
+    // -------------------
+    // Email Checking
+    // -------------------
+    const [ email, setEmail ] = useState<string | undefined>(undefined);
+    const [ showEmailPrompt, setShowEmailPrompt ] = useState(true);
+
+    useEffect(() => {
+        (async () => {
+            const ret = await Preferences.get({ key: 'email' });
+            const storedEmail = ret.value;
+            console.log('Retrieved email from storage: ', storedEmail);
+
+            setEmail(storedEmail ?? undefined);
+            setShowEmailPrompt(!(storedEmail && storedEmail.length > 0));
+
+            setIsLoading(false);
+        })();
+    }, []);
+
+
+    // -------------------
+    // Email Prompt Modal
+    // -------------------
+    const modal = useRef<HTMLIonModalElement>(null);
+    const input = useRef<HTMLIonInputElement>(null);
+
+    const [ emailInput, setEmailInput ] = useState('');
+
+    const handleEmailChange = (e: CustomEvent<InputChangeEventDetail>) => {
+        setEmailInput(e.detail.value || '');
+    };
+
+    function confirm() {
+        modal.current?.dismiss(input.current?.value, 'confirm');
+    }
+
+    const onEmailProvided = async (email: string) => {
+        await Preferences.set({ key: 'email', value: email });
+        setEmail(email);
+        setShowEmailPrompt(false);
+    };
 
     return (
         <IonPage>
@@ -20,20 +92,66 @@ export const Main: React.FC<MainProp> = ({ user}) => {
                     <IonButtons slot="start">
                         <IonMenuButton/>
                     </IonButtons>
-                    <IonTitle> Main Screen </IonTitle>
                 </IonToolbar>
             </IonHeader>
 
-            <IonContent fullscreen>
-                <IonText>Hello, your user ID is {user?.username}</IonText>
-                <IonText>Hello, {user?.attributes?.name}</IonText>
-                <IonText>Your email is {user?.attributes?.email}</IonText>
-                <IonText>Your phone number is {user?.attributes?.phone_number}</IonText>
-                <IonText>Your birthday is {user?.attributes?.birthdate}</IonText>
-                <IonText>Your picture is {user?.attributes?.picture}</IonText>
-                <ReactWebChat directLine={directLine}/>
-            </IonContent>
+            {
+                isLoading ? (
+                    <IonContent>
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                height: '100%',
+                            }}
+                        >
+                            <IonSpinner></IonSpinner>
+                        </div>
 
+                    </IonContent>
+                ) : (
+                    <IonContent scrollY={false}>
+                        {
+                            email ? <ReactWebChat
+                                    directLine={directLine}
+                                    styleOptions={styleOptions}
+                                    userID={email}
+                                />
+                                : <IonText>Please provide your email address to continue</IonText>
+                        }
+                        <IonModal ref={modal}
+                                  isOpen={showEmailPrompt}
+                                  onWillDismiss={(event) => onEmailProvided(event.detail.data)}>
+                            <IonHeader>
+                                <IonToolbar>
+                                    <IonButtons slot="end">
+                                        <IonButton strong={true}
+                                                   disabled={!emailInput}
+                                                   onClick={() => confirm()}>
+                                            Confirm
+                                        </IonButton>
+                                    </IonButtons>
+                                </IonToolbar>
+                            </IonHeader>
+                            <IonContent
+                                className="ion-padding"
+                                scrollY={false}
+                            >
+                                <IonText>Welcome to Vest, please enter your email address</IonText>
+                                <IonItem>
+                                    <IonInput ref={input}
+                                              aria-label="email"
+                                              type="email"
+                                              placeholder="example@gmail.com"
+                                              value={emailInput}
+                                              onIonChange={handleEmailChange}/>
+                                </IonItem>
+                            </IonContent>
+                        </IonModal>
+                    </IonContent>
+                )
+            }
         </IonPage>
     );
 };
